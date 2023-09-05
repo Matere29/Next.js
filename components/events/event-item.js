3d1b19a4f2514b811a5acf1ea6a22914b488cff0
Rorisang Makgaka <125877628+Matere29@users.noreply.github.com>
2023-09-05 20:05:03 +0200

    Add files via upload

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -2,31 +2,31 @@ import Link from 'next/link';
 import classes from './event-item.module.css'
 
 export default function EventItem(props) {
-    const {title, image, date, location, id} = props
-  const humanReadableDate = new Date(date).toLocaleDateString('en-US',{
+  const { title, image, date, location, id } = props
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
   })
   const formattedAddress = location.replace(',', '\n')
   const exploreLink = `/events/${id}`;
- 
- return <li> className={classes.item}
+
+  return <li className={classes.item}>
     <img src={'/' + image} alt={title} />
     <div className={classes.content}>
-        <div className={classes.summary}>
+      <div className={classes.summary}>
         <h2>{title}</h2>
         <div className={classes.date}>
-            <time>{humanReadableDate}</time>
+          <time>{humanReadableDate}</time>
         </div>
         <div className={classes.date}>
-            <address>{formattedAddress}</address>
+          <address>{formattedAddress}</address>
         </div>
-    </div>
-    <div className={classes.actions}>
+      </div>
+      <div className={classes.actions}>
         <Link href={exploreLink}>Explore Event</Link>
-    </div>
+      </div>
     </div>
   </li>
-  
+
 }

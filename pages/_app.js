554800fe14b2e384553d64570111b2d25d0ef568
Rorisang Makgaka <125877628+Matere29@users.noreply.github.com>
2023-09-06 14:36:53 +0200

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+import Layout from '@/components/events/layout/layout'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout>
+    <Component {...pageProps} />
+  </Layout>
 }
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import React from 'react'
 import { getFeaturedEvents } from '@/dummy-data';
 import EventList from '@/components/events/event.list';
 export default function HomePage(props) {
-    const { items } = props
+    
     const featuredEvents = getFeaturedEvents();
     return (
         <div>
+           
             <EventList items={featuredEvents} />
         </div>
     )
